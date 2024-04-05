a48df4a5e27b2b7e689e4d98f1d4842ad6a1870c
MOHIT VERMA <mohitverma3082002@gmail.com>
2024-04-05 16:46:47 +0530

    add files

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js") //require listing.js from models 
 const path = require("path"); //required path for the files
 const methodOverride = require("method-override");//require method-override (npm i method-override)
 const ejsMate = require("ejs-mate");//require ejs-mate >> help to create the layout the templete
+const wrapAsync = require("./utils/wrapAsync.js"); //For the Error-Handling MiddleWare to be required
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 
 //build the connections with mongoodb
@@ -37,11 +40,23 @@ app.get("/", (req,res) =>{
     res.send("Hello I'm coding my 1st Project")
 });
 
+//Middle-ware Schema
+const validateListing = (req,res,next) => {
+    let {error} = listingSchema.validate(req.body); 
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();
+    }
+} 
+
+
 // Index (Route)
-app.get("/listings", async(req,res) => {
+app.get("/listings", wrapAsync(async(req,res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-});
+}));
 
 // New (Route) for add the new listing
 app.get("/listings/new", (req,res) =>{
@@ -49,52 +64,48 @@ app.get("/listings/new", (req,res) =>{
 });
 
 // Show (Route)
-app.get("/listings/:id", async (req,res) =>{
+app.get("/listings/:id", wrapAsync(async (req,res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-});
+}));
 
-// Create (Route) for the New (Route) we created at line 38.
-app.post("/listings", async(req,res) => {
-   //let {title,description,image,price,location,country}  = req.body;  >> like this is also good but too large code for short see below
-   const newListing = new Listing(req.body.listing);
-   await newListing.save();
-   res.redirect("/listings");
-}) 
+// Create (Route) for the New (Route) we created at line 46.    //using wrapAsync for the Error-Handling Middle-ware
+app.post(
+    "/listings",
+    validateListing,
+    wrapAsync(async (req,res,next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");  
+   })
+);
 
 // Edit (Route) to edit the created list data
-app.get("/listings/:id/edit", async (req,res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing})
-})
+}));
 
 //Update (Route)
-app.put("/listings/:id", async (req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-});
+app.put(
+    "/listings/:id", 
+    validateListing,
+    wrapAsync(async (req,res) => {
+        let {id} = req.params;
+        await Listing.findByIdAndUpdate(id, {...req.body.listing});
+        res.redirect(`/listings/${id}`);
+    })
+);
 
 // DELETE (Route)
-app.delete("/listings/:id", async (req,res) => {
+app.delete("/listings/:id", wrapAsync(async (req,res) => {
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
-
-
-
-
-
-
-
-
-
-
-
+}));
 
 
 // testListing (Route)
@@ -112,6 +123,17 @@ app.delete("/listings/:id", async (req,res) => {
 // });
 
 
+// When you visit an undefine Route
+app.all("*", (req,res,next) => {
+    next(new ExpressError(404, "Sorry Page Not Found!!"));
+});
+
+//For Handling-Error we define the Middle-ware
+app.use((err,req,res,next) => {
+    let { statusCode = 500, message = "Something went Wrong!! Page Not Found"} = err;
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
+});
 
 
 //listenig on port 8080
