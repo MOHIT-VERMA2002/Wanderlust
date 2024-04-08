4b8b25e975ece0f831148cbb096dce7888aedb4b
MOHIT VERMA <mohitverma3082002@gmail.com>
2024-04-08 20:52:39 +0530

    add files

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const express = require("express"); //require Express
+const express = require("express"); 
 const app = express();
-const mongoose = require("mongoose");//require mongoose
-const Listing = require("./models/listing.js"); //require listing.js from models folder
-const path = require("path"); //required path for the files
-const methodOverride = require("method-override");//require method-override (npm i method-override)
-const ejsMate = require("ejs-mate");//require ejs-mate >> help to create the layout the templete
-const wrapAsync = require("./utils/wrapAsync.js"); //For the Error-Handling MiddleWare to be required
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js")
 
 
 //build the connections with mongoodb
@@ -25,15 +24,13 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 };
 
-//set up the views folder all (ejs) files
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));// for the overMethod 
 app.use(methodOverride("_method"));//use methodOverride
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));//use this code to access the css file from public folder
-// and we don't need to add the code in different use in only (boilerplate.ejs) file so it use in all 
-
+app.use(express.static(path.join(__dirname, "/public"))); 
 
 
 // Basic/Home (Route)
@@ -41,114 +38,8 @@ app.get("/", (req,res) =>{
     res.send("Hello I'm coding my 1st Project")
 });
 
-//Middle-ware Schema
-const validateListing = (req,res,next) => {
-    let {error} = listingSchema.validate(req.body); 
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else{
-        next();
-    }
-}; 
-
-//Review/Rating Schema
-const validateReview = (req,res,next) => {
-    let {error} = reviewSchema.validate(req.body); 
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else{
-        next();
-    }
-}; 
-
-
-// Index (Route)
-app.get("/listings", wrapAsync(async(req,res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-}));
-
-// New (Route) for add the new listing
-app.get("/listings/new", (req,res) =>{
-    res.render("listings/new.ejs");
-});
-
-// Show (Route)
-app.get("/listings/:id", wrapAsync(async (req,res) =>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-}));
-
-// Create (Route) for the New (Route) we created at line 46.    //using wrapAsync for the Error-Handling Middle-ware
-app.post(
-    "/listings",
-    validateListing,
-    wrapAsync(async (req,res,next) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");  
-   })
-);
-
-// Edit (Route) to edit the created list data
-app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing})
-}));
-
-//Update (Route)
-app.put(
-    "/listings/:id", 
-    validateListing,
-    wrapAsync(async (req,res) => {
-        let {id} = req.params;
-        await Listing.findByIdAndUpdate(id, {...req.body.listing});
-        res.redirect(`/listings/${id}`);
-    })
-);
-
-// DELETE (Route)
-app.delete("/listings/:id", wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-// Submitting the Review/Rating by [POST_Route]
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    // console.log("New Review Saved");
-    // res.send("New Review Saved");
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-
-// testListing (Route)
-// app.get("/testListing", async (req,res) => {
-//     let sampleListiing = new Listing({
-//         title: "My New Villa",
-//         description: "By the Beach",
-//         price: 1500,
-//         location: "Calangute Beach, Goa",
-//         country: "India",
-//     });
-//     await sampleListiing.save();
-//     console.log("Sample was Saved");
-//     res.send("Successful Testing");
-// });
+app.use("/listings", listings); //this is for all the Routes we created 
+app.use("/listings/:id/reviews", reviews);//this is for all the Review we created/Deleted
 
 
 // When you visit an undefine Route
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
   title: {
@@ -26,6 +27,16 @@ const listingSchema = new Schema({
     },
   ],
 });
+
+// Deleting the Reivew which are exists in listing Reviews array
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({_id: {$in: listing.reviews}});
+  }
+});
+
+
+
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
